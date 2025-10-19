44ea32305cc33d55088cd487ec0fab8f00502415
stephie-spec <compwork254@gmail.com>
2025-10-19 17:18:27 +0300

    complete codealong

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,17 +1,9 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useOutletContext, useParams } from "react-router-dom";
 
 function UserProfile() {
-  const [user, setUser] = useState({});
+  const users = useOutletContext();
   const params = useParams();
-  const userId = params.id;
-
-  useEffect(() =>{
-    fetch(`http://localhost:4000/users/${userId}`)
-    .then(r => r.json())
-    .then(data => setUser(data))
-    .catch(error => console.error(error));
-  }, [userId]);
 
   if(!user.name){
     return <h1>Loading...</h1>;
